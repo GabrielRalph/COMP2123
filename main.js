57c294dc7192cd15c89e1597dea2aed16f951c07
GabrielRalph <gltralph@gmail.com>
2020-03-24 03:38:05 +1100

    FIXED

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var list = ['A1', 'A2'];
 var html = ''
 
 for(var i in list){
-  html += '<h1 onclick = "window.open("https://comp2123.galetora.com/'+list[i]+'.html")>'+list[i]+'</h1>\n'
+  html += '<h1 onclick = "window.open(`https://comp2123.galetora.com/'+list[i]+'.html`)">'+list[i]+'</h1>\n'
+    console.log(html);
 }
 document.getElementById('app').innerHTML = html
