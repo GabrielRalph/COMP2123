a2f082b8f7ddf8c40f6a0a53e6694d973a8a38b7
GabrielRalph <gltralph@gmail.com>
2020-03-24 03:25:43 +1100

    dif

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var list = ['A1', 'A2'];
 var html = ''
 
 for(var i in list){
-  html += '<h1 onclick = "window.open(\'https://'+list[i].toLowerCase()+'.galetora.com\')">'+list[i]+'</h1>\n'
+  html += '<h1 onclick = "window.open("https://comp2123.galetora.com/'+list[i]+'.html")">'+list[i]+'</h1>\n'
 }
 document.getElementById('app').innerHTML = html
