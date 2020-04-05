a549095137a29f8f35403773109304b0acc1bcff
GabrielRalph <gltralph@gmail.com>
2020-04-06 00:37:05 +1000

    33

--- Waves/getWaves.js
+++ Waves/getWaves.js
@@ -1,35 +1,35 @@
 var ctrlpnl;
 var waves;
 
-// var pre = document.querySelector('pre');
-// var myScript = document.querySelector('script');
+var pre = document.querySelector('pre');
+var myScript = document.querySelector('script');
 
-// window.addEventListener('load', init, false);
-// function init() {
-//   var onswitch = document.querySelector('#switch');
-//   var wavesIn = document.querySelector('#waves-in');
-//   onswitch.style.setProperty('--running', '0');
-//   onswitch.addEventListener('click', function() {
-//     try {
-//       // Fix up for prefixing
-//       window.AudioContext = window.AudioContext||window.webkitAudioContext;
-//       ctrlpnl = new AudioContext();
-//       console.log(onswitch.style.getPropertyValue('--running'));
-//       if(onswitch.style.getPropertyValue('--running') === "0"){
-//         loadWaves(wavesIn);
-//         // waves.start(ctrlpnl.currentTime);
-//         onswitch.style.setProperty('--running', '1');
-//       }else{
-//         // waves.stop(ctrlpnl.currentTime);
-//         onswitch.style.setProperty('--running', '0');
-//       }
-//     }
-//     catch(e) {
-//       alert('Web Audio API is not supported in this browser' +e);
-//     }
-//
-//   });
-// }
+window.addEventListener('load', init, false);
+function init() {
+  var onswitch = document.querySelector('#switch');
+  var wavesIn = document.querySelector('#waves-in');
+  onswitch.style.setProperty('--running', '0');
+  onswitch.addEventListener('click', function() {
+    try {
+      // Fix up for prefixing
+      window.AudioContext = window.AudioContext||window.webkitAudioContext;
+      ctrlpnl = new AudioContext();
+      console.log(onswitch.style.getPropertyValue('--running'));
+      if(onswitch.style.getPropertyValue('--running') === "0"){
+        loadWaves(wavesIn);
+        // waves.start(ctrlpnl.currentTime);
+        onswitch.style.setProperty('--running', '1');
+      }else{
+        // waves.stop(ctrlpnl.currentTime);
+        onswitch.style.setProperty('--running', '0');
+      }
+    }
+    catch(e) {
+      alert('Web Audio API is not supported in this browser' +e);
+    }
+
+  });
+}
 
 function loadWaves(wavesInp) {
   waves = ctrlpnl.createMediaElementSource(wavesInp);
