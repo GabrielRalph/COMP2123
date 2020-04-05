f52eae47d2b31d822f86398f101c5f8ce8eac3b2
GabrielRalph <gltralph@gmail.com>
2020-04-06 02:09:17 +1000

    dis

--- Waves/getWaves.js
+++ Waves/getWaves.js
@@ -52,35 +52,12 @@
 // }
 //
 // // wire up buttons to stop and play audio
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var myAudio = document.querySelector('audio');
 
-// Create a MediaElementAudioSourceNode
-// Feed the HTMLMediaElement into it
-var source = audioCtx.createMediaElementSource(myAudio);
-
-// Create a gain node
-var gainNode = audioCtx.createGain();
-
-// Create variables to store mouse pointer Y coordinate
-// and HEIGHT of screen
-var CurY;
-var HEIGHT = window.innerHeight;
-
-// Get new mouse pointer coordinates when mouse is moved
-// then set new gain value
-
-document.ontouchmove = updatePage;
-
-function updatePage(e) {
-    CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
-    alert(CurY/HEIGHT)
-    gainNode.gain.value = CurY/HEIGHT;
-}
-
-
-// connect the AudioBufferSourceNode to the gainNode
-// and the gainNode to the destination, so we can play the
-// music and adjust the volume using the mouse cursor
-source.connect(gainNode);
-gainNode.connect(audioCtx.destination);
+window.addEventListener('load', ()=> {
+  var onswitch = document.querySelector('#switch');
+  var audio = document.getElementById('audio-input');
+  window.addEventListener('click', ()=>{
+    audio.play();
+    onswitch.style.setProperty('--running', '1');
+  })
+})
