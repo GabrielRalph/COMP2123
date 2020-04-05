6e200078e91cd3184ebc81369b5f49ff8d579b04
GabrielRalph <gltralph@gmail.com>
2020-04-06 01:51:56 +1000

    audio

--- Waves/getWaves.js
+++ Waves/getWaves.js
@@ -1,54 +1,86 @@
-var ctrlpnl;
-var waves;
+// var ctrlpnl;
+// var waves;
+//
+// var pre = document.querySelector('pre');
+// var myScript = document.querySelector('script');
+//
+// window.addEventListener('load', init, false);
+// function init() {
+//   var onswitch = document.querySelector('#switch');
+//   var wavesIn = document.getElementById('waves-in');
+//   onswitch.style.setProperty('--running', '0');
+//   onswitch.addEventListener('click', function() {
+//     try {
+//       // Fix up for prefixing
+//       window.AudioContext = window.AudioContext||window.webkitAudioContext;
+//       ctrlpnl = new AudioContext();
+//       console.log(onswitch.style.getPropertyValue('--running'));
+//       if(onswitch.style.getPropertyValue('--running') === "0"){
+//         loadWaves(wavesIn);
+//         // waves.start(ctrlpnl.currentTime);
+//         onswitch.style.setProperty('--running', '1');
+//       }else{
+//         // waves.stop(ctrlpnl.currentTime);
+//         onswitch.style.setProperty('--running', '0');
+//       }
+//     }
+//     catch(e) {
+//       alert('Web Audio API is not supported in this browser' +e);
+//     }
+//
+//   });
+// }
+//
+// function loadWaves(wavesInp) {
+//   waves = ctrlpnl.createMediaElementSource(wavesInp);
+//   var biquadFilter = ctrlpnl.createBiquadFilter();
+//   biquadFilter.frequency = 80;
+//   biquadFilter.type = "lowpass";
+//   var analyser = ctrlpnl.createAnalyser();
+//   waves.connect(biquadFilter);
+//   biquadFilter.connect(analyser);
+//
+//
+//   // ...
+//
+//   analyser.fftSize = 2048;
+//   var bufferLength = analyser.frequencyBinCount;
+//   var dataArray = new Uint8Array(bufferLength);
+//   analyser.getByteTimeDomainData(dataArray);
+//
+//   analyser.connect(ctrlpnl.destination)
+// }
+//
+// // wire up buttons to stop and play audio
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var myAudio = document.querySelector('audio');
 
-var pre = document.querySelector('pre');
-var myScript = document.querySelector('script');
+// Create a MediaElementAudioSourceNode
+// Feed the HTMLMediaElement into it
+var source = audioCtx.createMediaElementSource(myAudio);
 
-window.addEventListener('load', init, false);
-function init() {
-  var onswitch = document.querySelector('#switch');
-  var wavesIn = document.getElementById('waves-in');
-  onswitch.style.setProperty('--running', '0');
-  onswitch.addEventListener('click', function() {
-    try {
-      // Fix up for prefixing
-      window.AudioContext = window.AudioContext||window.webkitAudioContext;
-      ctrlpnl = new AudioContext();
-      console.log(onswitch.style.getPropertyValue('--running'));
-      if(onswitch.style.getPropertyValue('--running') === "0"){
-        loadWaves(wavesIn);
-        // waves.start(ctrlpnl.currentTime);
-        onswitch.style.setProperty('--running', '1');
-      }else{
-        // waves.stop(ctrlpnl.currentTime);
-        onswitch.style.setProperty('--running', '0');
-      }
-    }
-    catch(e) {
-      alert('Web Audio API is not supported in this browser' +e);
-    }
+// Create a gain node
+var gainNode = audioCtx.createGain();
 
-  });
+// Create variables to store mouse pointer Y coordinate
+// and HEIGHT of screen
+var CurY;
+var HEIGHT = window.innerHeight;
+
+// Get new mouse pointer coordinates when mouse is moved
+// then set new gain value
+
+document.onmousemove = updatePage;
+
+function updatePage(e) {
+    CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+
+    gainNode.gain.value = CurY/HEIGHT;
 }
 
-function loadWaves(wavesInp) {
-  waves = ctrlpnl.createMediaElementSource(wavesInp);
-  var biquadFilter = ctrlpnl.createBiquadFilter();
-  biquadFilter.frequency = 80;
-  biquadFilter.type = "lowpass";
-  var analyser = ctrlpnl.createAnalyser();
-  waves.connect(biquadFilter);
-  biquadFilter.connect(analyser);
 
-
-  // ...
-
-  analyser.fftSize = 2048;
-  var bufferLength = analyser.frequencyBinCount;
-  var dataArray = new Uint8Array(bufferLength);
-  analyser.getByteTimeDomainData(dataArray);
-
-  analyser.connect(ctrlpnl.destination)
-}
-
-// wire up buttons to stop and play audio
+// connect the AudioBufferSourceNode to the gainNode
+// and the gainNode to the destination, so we can play the
+// music and adjust the volume using the mouse cursor
+source.connect(gainNode);
+gainNode.connect(audioCtx.destination);
